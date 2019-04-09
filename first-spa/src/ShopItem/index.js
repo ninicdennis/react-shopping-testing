22f543a799caaae63752732b05cdd769b8a916e4
Pupperbot <pupperbot@protonmail.com>
2019-04-09 13:52:06 -0600

    improved shop page with image and side text.

--- first-spa/src/ShopItem/index.js
+++ first-spa/src/ShopItem/index.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import NormalHead from '../mainpage/header/index'
 import './styles.css';
+import { Image, Header } from 'semantic-ui-react'
 
 class ShopItem extends Component {
   render() {
     return(
     <div>
       <NormalHead />
+      <div className='item-body'>
+      <div className='itemImage'>
+        <Image src={require('../images/image.jpg')} alt = 'sample' size='large'/>
+      </div>
+      <div className='itemDesc'>
+        <Header as='h3'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Integer lobortis turpis in hendrerit ornare. Nam viverra quam quis scelerisque consectetur.
+          Donec et dui at libero eleifend auctor. Aliquam vestibulum ex sed velit rutrum, ut convallis nunc volutpat. 
+          Phasellus a euismod augue. Etiam cursus metus sed est gravida, in pharetra neque viverra. 
+          Phasellus sed faucibus leo. Sed dictum ligula eros, eget fermentum leo tempor mattis. 
+          Sed ac magna quis odio bibendum pretium.
+        </Header>
+      </div>
+      </div>
+      <div>
+        hello
+      </div>
     </div>
     )
   }
