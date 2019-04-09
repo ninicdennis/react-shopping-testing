87e2640031a212834ac361d76b66f3f22702b65e
Pupperbot <pupperbot@protonmail.com>
2019-04-08 23:56:22 -0600

    added shop page and is now accessible.

--- first-spa/src/App.js
+++ first-spa/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import ShopPage from './shopPage'
 import FrontPage from './mainpage/index'
+import ShopItem from './ShopItem/index'
 
 class App extends Component {
   constructor(props) {
@@ -26,7 +27,11 @@ class App extends Component {
         </label>
         <label>
         <input type="radio" name="pageChange" value="Shop" onClick={this.updatePage} />
-        Shop Page
+        ShopItem
+        </label>
+        <label>
+        <input type="radio" name="pageChange" value="ItemPage" onClick={this.updatePage} /> 
+        Item Page
         </label>
         <FrontPage /></div>
       case 'Shop':
@@ -39,7 +44,27 @@ class App extends Component {
       <input type="radio" name="pageChange" value="Shop" onClick={this.updatePage} />
       Shop Page
       </label>
+      <label>
+        <input type="radio" name="pageChange" value="ItemPage" onClick={this.updatePage} /> 
+        </label>
+        Item Page
         <ShopPage /></div>
+        
+        case 'ItemPage':
+        return <div>
+        <label>
+        <input type="radio" name="pageChange" value="Front" onClick={this.updatePage} />
+        Front Page
+        </label>
+        <label>
+        <input type="radio" name="pageChange" value="Shop" onClick={this.updatePage} />
+        Shop Page
+        </label>
+        <label>
+        <input type="radio" name="pageChange" value="ItemPage" onClick={this.updatePage} /> 
+        Item Page
+        </label>
+          <ShopItem /></div>
       default:
       return <div>Page broke</div>
     }
---
+++ first-spa/src/ShopItem/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NormalHead from '../mainpage/header/index'
+import './styles.css';
+
+class ShopItem extends Component {
+  render() {
+    return(
+    <div>
+      <NormalHead />
+    </div>
+    )
+  }
+}
+
+export default ShopItem;
+
